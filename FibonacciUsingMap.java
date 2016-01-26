d1afd02873a21c51b14b9f1490a2fe223faedd96
Chris Kimberley <ckimbe02@mail.bbk.ac.uk>
2016-01-26 16:15:14 +0000

    passed in map as an argument

--- FibonacciUsingMap.java
+++ FibonacciUsingMap.java
@@ -1,10 +1,37 @@
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class FibonacciUsingMap {
-    private static Map<Long, Long> fibonacciMap = new ConcurrentHashMap<>();
+    //private static Map<Long, Long> fibonacciMap = new ConcurrentHashMap<>();
 
-	public static long FibonacciCalc(long n) {
+    public static void main (String [] args) {
+		FibonacciUsingMap fum = new FibonacciUsingMap();
+		fum.launch();
+	}
+
+	private void launch() {
+		System.out.print("FibonacciUsingMap - ENTER n: ");
+		System.out.println(FibonacciCalc(new Scanner(System.in).nextInt()));
+	}
+
+	public static long FibonacciCalc (long n) {
+		return FibMapCalc(n, new ConcurrentHashMap<>());
+	}
+
+	private static long FibMapCalc(long n, Map<Long, Long> fibMap) {
+	long fibResult;
+		if ((n == 1) || (n == 2)) {
+			return 1;
+		}
+		if (fibMap.containsKey(n)) {
+			return fibMap.get(n);
+		}	
+		fibResult = FibMapCalc(n-1, fibMap) + FibMapCalc(n-2, fibMap);
+		fibMap.put(n, fibResult);
+		return (fibResult);
+	}
+
+	/*public static long FibonacciCalc(long n) {
 	long fibResult;
 		if ((n == 1) || (n == 2)) {
 			return 1;
@@ -15,5 +42,5 @@ public class FibonacciUsingMap {
 			fibonacciMap.put(n, fibResult);
 			return (fibResult);
 		}
-	}
+	}*/
 }
